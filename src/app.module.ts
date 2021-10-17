a0bad1aee9a80f23201e4868e22cd368994422c0
ganeshs1461 <ganesh@keyvalue.systems>
2021-10-17 13:37:34 +0530

    program to create graphql definitions

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
+import { AppGraphQLModule } from './graphql/graphql.module';
 
 @Module({
   imports: [DatabaseModule, ConfigModule.forRoot()],
