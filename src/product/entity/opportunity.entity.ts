d124755ee78d665dbcdda08f06e3c8651674e041
ganeshs1461 <ganesh@keyvalue.systems>
2021-10-18 13:17:54 +0530

    added opportunity entity

--- src/product/entity/opportunity.entity.ts
+++ src/product/entity/opportunity.entity.ts
@@ -1,8 +1,30 @@
-import { Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Risk } from '../../schema/graphql.schema';
 import { BaseEntity } from './base.entity';
 
 @Entity()
 export class Opportunity extends BaseEntity {
   @PrimaryGeneratedColumn('uuid')
   id!: string;
+
+  @Column()
+  name!: string;
+
+  @Column()
+  account!: string;
+
+  @Column({ type: 'decimal', precision: 5, scale: 2, default: 0 })
+  winPercentage?: number;
+
+  @Column({ nullable: true })
+  primaryContact?: string;
+
+  @Column({ nullable: true })
+  closeDate?: Date;
+
+  @Column({ nullable: true })
+  estimatedRevenue?: string;
+
+  @Column({ nullable: true })
+  riskLevel?: Risk;
 }
