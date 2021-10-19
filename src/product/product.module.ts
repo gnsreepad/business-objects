a89f0f829d75fe0421f782537020a4bba3a064c7
ganeshs1461 <ganesh@keyvalue.systems>
2021-10-19 12:49:50 +0530

    injected opportunity resolver and service to product module

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -3,10 +3,17 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Contact } from './entity/contact.entity';
 import { Opportunity } from './entity/opportunity.entity';
 import { ContactResolver } from './resolver/contact.resolver';
+import { OpportunityResolver } from './resolver/opportunity.resolver';
 import { ContactSercie } from './service/contact.service';
+import { OpportunityService } from './service/opportunity.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Contact, Opportunity])],
-  providers: [ContactSercie, ContactResolver],
+  providers: [
+    ContactSercie,
+    ContactResolver,
+    OpportunityService,
+    OpportunityResolver,
+  ],
 })
 export class ProductModule {}
